feb05b7d0794ab79c9928cee78c693462f2d567b
lorandsandor <lorand.sandor@sage.com>
2018-07-09 19:59:09 +0100

    Tidy up

--- lib/external/v1/article_list.rb
+++ lib/external/v1/article_list.rb
@@ -4,12 +4,15 @@ module External
       include HTTParty
 
       def initialize
+        # Get base uri or default
         @base_uri = ENV.fetch(
             'ARTICLES_LIST_URI',
             'https://s3-eu-west-1.amazonaws.com/olio-staging-images/developer'
         )
       end
 
+      # Get article list
+      # @returns Array
       def get_articles
         response = HTTParty.get(article_list_url)
         handle_failure(response.code)
@@ -19,15 +22,19 @@ module External
 
       private
 
+      # Raise error if response is not success
       def handle_failure(response_code)
         return if response_code == 200
+
         raise "[#{self.class}] - Unsuccessful response. Code: #{response_code}"
       end
 
+      # Collate url for article list
       def article_list_url
         @base_uri + '/articles.json'
       end
 
+      # Format output to hash
       def to_hash(payload)
         JSON.parse(payload)
       end
--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -2,11 +2,18 @@ require 'rails_helper'
 
 RSpec.describe ArticlesController, type: :controller do
   let(:article) { Article.new(id: 1, likes: 0) }
+  let(:list) { [{id: 1, sutff: 'more_stuff' }] }
+  let(:article_list) { double('article_list', get_articles: list ) }
+
+
+  before do
+    allow(External::V1::ArticleList).to receive(:new).and_return article_list
+  end
 
   describe 'index' do
     it 'sets the article list' do
       get :index
-      expect(subject.instance_variable_get(:@articles)).to eq []
+      expect(subject.instance_variable_get(:@articles)).to eq list
     end
   end
 
